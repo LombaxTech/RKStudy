125e50f52ca9eeb323053c8f19db43c0dc626085
Rakib <rakib@think-engineer.com>
2024-03-07 14:36:45 +0000

    profile page

---
+++ pages/my-profile.tsx
@@ -0,0 +1,250 @@
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  Fragment,
+} from "react";
+import { AuthContext } from "@/context/AuthContext";
+import {
+  db,
+  auth,
+  // storage
+} from "@/firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  deleteUser,
+  getAuth,
+  reauthenticateWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "next/router";
+
+export default function MyProfile() {
+  const { user, setUser } = useContext(AuthContext);
+
+  const [name, setName] = useState<any>(user?.name || user?.displayName);
+  const [about, setAbout] = useState<string>(user?.about);
+  const [school, setSchool] = useState<any>(null);
+
+  const [changesExist, setChangesExist] = useState(false);
+
+  const [changesSaved, setChangesSaved] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [deleteAccountModalOpen, setDeleteAccountModalOpen] = useState(false);
+
+  useEffect(() => {
+    const initSchool = async () => {
+      try {
+        let school: any = null;
+
+        let schoolsSnapshot = await getDocs(collection(db, "schools"));
+        schoolsSnapshot.forEach((schoolDoc) => {
+          if (schoolDoc.id === user.schoolId)
+            school = { id: schoolDoc.id, ...schoolDoc.data() };
+        });
+
+        setSchool(school);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (user && user.schoolId) initSchool();
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    if (user.name !== name || user.about !== about) {
+      setChangesExist(true);
+    } else {
+      setChangesExist(false);
+    }
+  }, [user, name, about]);
+
+  const saveChanges = async () => {
+    setError(false);
+    setChangesSaved(false);
+
+    if (!changesExist) return;
+
+    try {
+      await updateDoc(doc(db, "users", user.uid), { name, about });
+      setChangesSaved(true);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  if (user)
+    return (
+      <div className="p-10 flex flex-col items-center">
+        <div className="flex flex-col gap-6 lg:w-4/12 w-full">
+          <h1 className="font-bold text-xl">Edit Profile</h1>
+          <div className="flex flex-col gap-2">
+            <label>Name: </label>
+            <input
+              disabled={true}
+              type="text"
+              className="border outline-none p-2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="name"
+            />
+          </div>
+          {school ? (
+            <div className="flex flex-col gap-2">
+              <label>School: </label>
+              <input
+                disabled={true}
+                type="text"
+                className="border outline-none p-2"
+                value={school.name}
+                placeholder="name"
+              />
+            </div>
+          ) : (
+            <div className="">
+              <h1 className="">You have not joined any schools</h1>
+            </div>
+          )}
+          {/* <button
+            disabled={!changesExist}
+            className="btn btn-primary"
+            onClick={saveChanges}
+          >
+            Save Changes
+          </button> */}
+
+          {error && (
+            <div className="bg-red-200 p-2 text-red-800 text-center">
+              Something went wrong. Please try again.
+            </div>
+          )}
+          {changesSaved && (
+            <div className="bg-green-200 p-2 text-green-800 text-center">
+              Changes saved successfully.
+            </div>
+          )}
+
+          {/* <h1
+            className="cursor-pointer"
+            onClick={() => setDeleteAccountModalOpen(true)}
+          >
+            Delete Account
+          </h1> */}
+
+          {/* <DeleteAccountModal
+            deleteAccountModalOpen={deleteAccountModalOpen}
+            setDeleteAccountModalOpen={setDeleteAccountModalOpen}
+          /> */}
+        </div>
+      </div>
+    );
+}
+
+// function DeleteAccountModal({
+//   deleteAccountModalOpen,
+//   setDeleteAccountModalOpen,
+// }: {
+//   deleteAccountModalOpen: any;
+//   setDeleteAccountModalOpen: any;
+// }) {
+//   function closeModal() {
+//     setDeleteAccountModalOpen(false);
+//   }
+
+//   function openModal() {
+//     setDeleteAccountModalOpen(true);
+//   }
+
+//   const { user } = useContext(AuthContext);
+//   const router = useRouter();
+
+//   const deleteAccount = async () => {
+//     try {
+//       await deleteDoc(doc(db, "users", user.uid));
+//       await signOut(auth);
+//       router.push("/");
+
+//       console.log("deleted account...");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     <Transition appear show={deleteAccountModalOpen} as={Fragment}>
+//       <Dialog as="div" className="relative z-10" onClose={closeModal}>
+//         <Transition.Child
+//           as={Fragment}
+//           enter="ease-out duration-300"
+//           enterFrom="opacity-0"
+//           enterTo="opacity-100"
+//           leave="ease-in duration-200"
+//           leaveFrom="opacity-100"
+//           leaveTo="opacity-0"
+//         >
+//           <div className="fixed inset-0 bg-black/25" />
+//         </Transition.Child>
+
+//         <div className="fixed inset-0 overflow-y-auto">
+//           <div className="flex min-h-full items-center justify-center p-4 text-center">
+//             <Transition.Child
+//               as={Fragment}
+//               enter="ease-out duration-300"
+//               enterFrom="opacity-0 scale-95"
+//               enterTo="opacity-100 scale-100"
+//               leave="ease-in duration-200"
+//               leaveFrom="opacity-100 scale-100"
+//               leaveTo="opacity-0 scale-95"
+//             >
+//               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+//                 <Dialog.Title
+//                   as="h3"
+//                   className="text-lg font-medium leading-6 text-gray-900"
+//                 >
+//                   Account Deletion Confirmation
+//                 </Dialog.Title>
+//                 <div className="mt-2">
+//                   <p className="text-sm text-gray-500">
+//                     Are you sure you want to delete your account? This action is
+//                     irreversible.
+//                   </p>
+//                 </div>
+
+//                 <div className="mt-4 flex gap-2">
+//                   {/* <button
+//                     type="button"
+//                     className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+//                     onClick={closeModal}
+//                   >
+//                     Got it, thanks!
+//                   </button> */}
+//                   <button className="btn" onClick={deleteAccount}>
+//                     Delete
+//                   </button>
+//                   <button className="btn" onClick={closeModal}>
+//                     Cancel
+//                   </button>
+//                 </div>
+//               </Dialog.Panel>
+//             </Transition.Child>
+//           </div>
+//         </div>
+//       </Dialog>
+//     </Transition>
+//   );
+// }
