79b818152588ae275116a895569a49e30f5f29af
Rakib <rakib@think-engineer.com>
2024-03-09 14:48:03 +0000

    student quizzes page layout change

--- components/StudentHome.tsx
+++ components/StudentHome.tsx
@@ -65,65 +65,95 @@ export default function StudentHome() {
   if (user)
     return (
       <>
-        <div className="flex-1 flex flex-col items-center p-4 pt-12">
-          <div className="flex flex-col gap-4 w-full px-10">
-            <div className="flex items-center justify-between mb-10">
-              <div className="flex items-center gap-8">
-                <h1 className="text-2xl font-bold">
-                  {typeOfQuizzesToShow === "All" && "All Quizzes"}
-                  {typeOfQuizzesToShow === "My Quizzes" && "My Quizzes"}
-                </h1>
+        <div className="flex-1 flex gap-4 px-8 pt-12">
+          {/* TITLE + FILTER */}
 
-                <select
-                  className="select border outline"
-                  value={typeOfQuizzesToShow}
-                  // @ts-ignore
-                  onChange={(e) => setTypeOfQuizzesToShow(e.target.value)}
-                >
-                  <option>All</option>
-                  <option>My Quizzes</option>
-                </select>
-              </div>
+          <div className="flex flex-col gap-4 w-3/12">
+            {/* TITLE */}
+            <div className="p-8 bg-white rounded-md shadow flex flex-col gap-8">
+              <h1 className="text-2xl font-bold">Welcome back {user.name}</h1>
               <button
-                className="btn"
+                className="btn btn-primary"
                 onClick={() => setCreateQuizModalIsOpen(true)}
               >
-                Create Quiz
+                Create New Quiz
               </button>
             </div>
 
-            {/* <h1 className="text-2xl font-bold">Welcome back {user.name}</h1> */}
+            {/* TITLE */}
+            <div className="p-8 bg-white rounded-md shadow flex flex-col gap-2">
+              <h1 className="text-xl font-medium mb-4">Filter Quizzes</h1>
 
-            {/* QUIZZES */}
-            {quizzes &&
-              quizzes.map((quiz: any, i: any) => {
-                const isMyQuiz = quiz.createdBy.userId === user.uid;
+              {/* WHO'S QUIZZES */}
+              <select
+                className="p-2 pr-4 border outline"
+                value={typeOfQuizzesToShow}
+                // @ts-ignore
+                onChange={(e) => setTypeOfQuizzesToShow(e.target.value)}
+              >
+                <option>All</option>
+                <option>My Quizzes</option>
+              </select>
 
-                if (typeOfQuizzesToShow === "My Quizzes" && !isMyQuiz)
-                  return null;
+              {/* SUBJECT OF QUIZZES */}
 
-                if (!isMyQuiz && !quiz.public) return null;
+              {/* <div className="flex items-center justify-between mb-10">
+            <div className="flex items-center gap-8">
+              <h1 className="text-2xl font-bold">
+                {typeOfQuizzesToShow === "All" && "All Quizzes"}
+                {typeOfQuizzesToShow === "My Quizzes" && "My Quizzes"}
+              </h1>
 
-                return (
-                  <div className="p-2 border-2 shadow-sm">
-                    <h1 className="">{quiz.title}</h1>
-                    <span className="">Made by: {quiz.createdBy.name}</span>
-                    <div className="flex items-center gap-2">
-                      <Link href={`/quiz/${quiz.id}`}>
-                        <button className="btn">Take Quiz</button>
-                      </Link>
-                      {isMyQuiz && (
-                        <button
-                          className="btn"
-                          onClick={() => deleteQuiz(quiz)}
-                        >
-                          Delete Quiz
-                        </button>
-                      )}
+            </div>
+            <button
+              className="btn"
+              onClick={() => setCreateQuizModalIsOpen(true)}
+            >
+              Create Quiz
+            </button>
+          </div> */}
+            </div>
+          </div>
+
+          {/* QUIZZES */}
+          <div className="flex flex-col gap-2 w-9/12">
+            {/* <div className="flex items-center justify-between">
+              <h1 className="text-2xl font-bold">Quizzes</h1>
+              <button className="btn btn-secondary btn-sm">
+                Generate Quiz
+              </button>
+            </div> */}
+            <div className="flex flex-col gap-2">
+              {quizzes &&
+                quizzes.map((quiz: any, i: any) => {
+                  const isMyQuiz = quiz.createdBy.userId === user.uid;
+
+                  if (typeOfQuizzesToShow === "My Quizzes" && !isMyQuiz)
+                    return null;
+
+                  if (!isMyQuiz && !quiz.public) return null;
+
+                  return (
+                    <div className="p-2 border-2 shadow-sm rounded-md bg-white">
+                      <h1 className="">{quiz.title}</h1>
+                      <span className="">Made by: {quiz.createdBy.name}</span>
+                      <div className="flex items-center gap-2">
+                        <Link href={`/quiz/${quiz.id}`}>
+                          <button className="btn">Take Quiz</button>
+                        </Link>
+                        {isMyQuiz && (
+                          <button
+                            className="btn"
+                            onClick={() => deleteQuiz(quiz)}
+                          >
+                            Delete Quiz
+                          </button>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+            </div>
 
             {quizzes.length === 0 ? (
               <div className="text-lg font-medium">
@@ -131,6 +161,8 @@ export default function StudentHome() {
               </div>
             ) : null}
           </div>
+
+          {/* <h1 className="text-2xl font-bold">Welcome back {user.name}</h1> */}
         </div>
         <CreateQuizModal
           createQuizModalIsOpen={createQuizModalIsOpen}
@@ -159,6 +191,10 @@ function CreateQuizModal({
   const [quizAbout, setQuizAbout] = useState("");
   const [isQuizPrivate, setIsQuizPrivate] = useState<"Yes" | "No">("No");
 
+  const [creatingQuiz, setCreatingQuiz] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const [file, setFile] = useState<any>(null);
 
   const handleFileChange = (e: any) => {
@@ -166,6 +202,10 @@ function CreateQuizModal({
   };
 
   const createQuiz = async () => {
+    setError("");
+    setSuccess(false);
+    setCreatingQuiz(true);
+
     try {
       console.log("creating quiz");
 
@@ -228,16 +268,26 @@ function CreateQuizModal({
 
         setQuizTitle("");
         setQuizAbout("");
+        setSuccess(true);
+        setCreatingQuiz(false);
         closeModal();
       };
     } catch (error) {
       console.log(error);
+      setError("Something went wrong");
+      setCreatingQuiz(false);
     }
   };
 
   return (
     <Transition appear show={createQuizModalIsOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => {
+          if (!creatingQuiz) closeModal();
+        }}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -261,68 +311,80 @@ function CreateQuizModal({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Create Quiz
-                </Dialog.Title>
-
-                {/* CREATE QUIZ */}
-                <div className="flex flex-col gap-2 mt-4">
-                  <input
-                    type="text"
-                    placeholder="Enter quiz title"
-                    className="p-1 outline-none border"
-                    value={quizTitle}
-                    onChange={(e) => setQuizTitle(e.target.value)}
-                  />
-                  <textarea
-                    placeholder="What should the quiz be about?"
-                    className="p-2 outline-none border"
-                    value={quizAbout}
-                    onChange={(e) => setQuizAbout(e.target.value)}
-                  />
-
-                  <div className="flex items-center gap-3">
-                    <label className="w-8/12">Make quiz private? </label>
-                    <select
-                      className="select w-full max-w-xs border"
-                      value={isQuizPrivate}
-                      // @ts-ignore
-                      onChange={(e) => setIsQuizPrivate(e.target.value)}
-                    >
-                      <option>Yes</option>
-                      <option>No</option>
-                    </select>
+              {creatingQuiz ? (
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex flex-col items-center gap-2">
+                    <span className="loading loading-spinner loading-lg"></span>
+                    <h1 className="">Creating Quiz. Please wait a moment</h1>
                   </div>
-
-                  {/* UPLOAD FILES TO GENERATE QUIZ FROM */}
-                  <div className="">
-                    <input type="file" onChange={handleFileChange} />
-
-                    {file && (
-                      <div className="flex flex-col justify-center items-center mt-4">
-                        <span className="">
-                          <FaFile size={30} />
-                        </span>
-                        <span className="">{file.name}</span>
-                        <button className="btn" onClick={() => setFile(null)}>
-                          Remove
-                        </button>
-                      </div>
-                    )}
-                  </div>
-
-                  <button
-                    className="btn btn-sm btn-primary"
-                    onClick={createQuiz}
+                </Dialog.Panel>
+              ) : (
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
                   >
                     Create Quiz
-                  </button>
-                </div>
-              </Dialog.Panel>
+                  </Dialog.Title>
+
+                  {/* CREATE QUIZ */}
+                  <div className="flex flex-col gap-2 mt-4">
+                    <input
+                      type="text"
+                      placeholder="Enter quiz title"
+                      className="p-1 outline-none border"
+                      value={quizTitle}
+                      onChange={(e) => setQuizTitle(e.target.value)}
+                    />
+                    <textarea
+                      placeholder="What is the quiz about?"
+                      className="p-2 outline-none border"
+                      value={quizAbout}
+                      onChange={(e) => setQuizAbout(e.target.value)}
+                    />
+
+                    <div className="flex items-center gap-3">
+                      <label className="w-8/12">Make quiz private? </label>
+                      <select
+                        className="select w-full max-w-xs border"
+                        value={isQuizPrivate}
+                        // @ts-ignore
+                        onChange={(e) => setIsQuizPrivate(e.target.value)}
+                      >
+                        <option>Yes</option>
+                        <option>No</option>
+                      </select>
+                    </div>
+
+                    {/* UPLOAD FILES TO GENERATE QUIZ FROM */}
+                    <div className="flex flex-col gap-2 my-4">
+                      <label className="">
+                        Upload files to generate quiz from
+                      </label>
+                      <input type="file" onChange={handleFileChange} />
+
+                      {file && (
+                        <div className="flex flex-col justify-center items-center mt-4">
+                          <span className="">
+                            <FaFile size={30} />
+                          </span>
+                          <span className="">{file.name}</span>
+                          <button className="btn" onClick={() => setFile(null)}>
+                            Remove
+                          </button>
+                        </div>
+                      )}
+                    </div>
+
+                    <button
+                      className="btn btn-sm btn-primary"
+                      onClick={createQuiz}
+                    >
+                      Create Quiz
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              )}
             </Transition.Child>
           </div>
         </div>
