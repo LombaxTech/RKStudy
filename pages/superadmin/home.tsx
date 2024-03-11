784ddfb9c7ef123b29dd4c40dfc8b70067d05820
Rakib <rakib@think-engineer.com>
2024-03-11 00:19:42 +0000

    super admin pages

--- pages/superadmin/home.tsx
+++ pages/superadmin/home.tsx
@@ -2,7 +2,9 @@ import { AuthContext } from "@/context/AuthContext";
 import { db } from "@/firebase";
 import { addDoc, collection, doc, getDoc, getDocs } from "firebase/firestore";
 import Link from "next/link";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { generateInviteCode } from "@/helperFunctions";
 
 type School = any;
 
@@ -10,10 +12,7 @@ export default function SuperAdminHome() {
   const { user } = useContext(AuthContext);
 
   const [schools, setSchools] = useState<School[]>([]);
-
-  const [schoolName, setSchoolName] = useState("");
-  const [licenseLimit, setLicenseLimit] = useState<any>(0);
-  const [admins, setAdmins] = useState<any>([]);
+  const [createSchoolModalIsOpen, setCreateSchoolModalIsOpen] = useState(false);
 
   useEffect(() => {
     const fetchSchools = async () => {
@@ -32,8 +31,138 @@ export default function SuperAdminHome() {
     if (user) fetchSchools();
   }, [user]);
 
+  const logStuff = () => {
+    console.log(schools);
+  };
+
+  if (user && user.type === "superadmin")
+    return (
+      <>
+        <div className="flex-1 flex flex-col lg:flex-row gap-4 px-8 pt-12">
+          {/* TITLE + FILTER */}
+
+          <div className="flex flex-col gap-4 lg:w-3/12 w-full">
+            {/* TITLE */}
+            <div className="p-8 bg-white rounded-md shadow flex flex-col gap-8">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-2xl font-bold">Welcome back {user.name}</h1>
+                <h1 className="text-xl font-medium text-center">SuperAdmin</h1>
+              </div>
+
+              <button
+                className="btn btn-primary"
+                onClick={() => setCreateSchoolModalIsOpen(true)}
+              >
+                Create New School
+              </button>
+            </div>
+
+            {/* TITLE */}
+            <div className="p-8 bg-white rounded-md shadow flex flex-col gap-2">
+              {/* <h1 className="text-xl font-medium mb-4">Filter Quizzes</h1> */}
+
+              {/* WHO'S QUIZZES */}
+              {/* <select
+              className="p-2 pr-4 border outline"
+              value={typeOfQuizzesToShow}
+              // @ts-ignore
+              onChange={(e) => setTypeOfQuizzesToShow(e.target.value)}
+            >
+              <option>All</option>
+              <option>My Quizzes</option>
+            </select> */}
+
+              {/* SUBJECT OF QUIZZES */}
+            </div>
+          </div>
+
+          {/* SCHOOLS */}
+          <div className="flex flex-col gap-2 lg:w-9/12 w-full">
+            {schools &&
+              schools.map((school: any) => {
+                return (
+                  <div
+                    key={school.id}
+                    className="p-2 border flex items-center justify-between bg-white shadow-md"
+                  >
+                    <h1 className="">{school.name}</h1>
+                    <div className="flex gap-2">
+                      <Link href={`/superadmin/schools/${school.id}`}>
+                        <button className="btn btn-primary btn-sm">View</button>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+
+            {schools.length === 0 ? (
+              <h1 className="">No schools to show...</h1>
+            ) : null}
+          </div>
+
+          {/* <h1 className="text-2xl font-bold">Welcome back {user.name}</h1> */}
+        </div>
+        <CreateSchoolModal
+          createSchoolModalIsOpen={createSchoolModalIsOpen}
+          setCreateSchoolModalIsOpen={setCreateSchoolModalIsOpen}
+          setSchools={setSchools}
+        />
+      </>
+    );
+
+  // return (
+
+  //   <div className="flex-1 flex flex-col gap-4 p-10">
+  //     <button className="btn" onClick={logStuff}>
+  //       Log stuff
+  //     </button>
+
+  //     {/* CREATE SCHOOL */}
+  //     <div className="p-6 rounded-md shadow-md flex flex-col gap-4">
+  //     </div>
+
+  //     {/* VIEW SCHOOLS */}
+  //     <h1 className="font-bold">Schools</h1>
+  //     {schools &&
+  //       schools.map((school: School) => {
+  //         return (
+  //           <Link href={`/superadmin/schools/${school.id}`} key={school.id}>
+  //             <div className="p-2 border">
+  //               <h1 className="">{school.name}</h1>
+  //             </div>
+  //           </Link>
+  //         );
+  //       })}
+
+  //     {schools.length === 0 ? (
+  //       <h1 className="">No schools to show...</h1>
+  //     ) : null}
+  //     {/* VIEW STUDENTS */}
+  //   </div>
+  // );
+}
+
+function CreateSchoolModal({
+  createSchoolModalIsOpen,
+  setCreateSchoolModalIsOpen,
+  setSchools,
+}: {
+  createSchoolModalIsOpen: any;
+  setCreateSchoolModalIsOpen: any;
+  setSchools: any;
+}) {
+  const closeModal = () => setCreateSchoolModalIsOpen(false);
+  const openModal = () => setCreateSchoolModalIsOpen(true);
+
+  const { user } = useContext(AuthContext);
+
+  const [schoolName, setSchoolName] = useState("");
+  const [licenseLimit, setLicenseLimit] = useState<any>(100);
+  const [admins, setAdmins] = useState<any>([]);
+
   const createSchool = async () => {
     try {
+      // create school
       let newSchool = {
         name: schoolName,
         licenseLimit,
@@ -44,72 +173,134 @@ export default function SuperAdminHome() {
 
       let newSchoolDoc = await addDoc(collection(db, "schools"), newSchool);
 
-      console.log("created new school");
+      setSchools((oldSchools: any) => [
+        ...oldSchools,
+        { id: newSchoolDoc.id, ...newSchool },
+      ]);
 
-      setSchools([...schools, { id: newSchoolDoc.id, ...newSchool }]);
+      // create teacher invite code
+      const teacherInvite = {
+        school: {
+          id: newSchoolDoc.id,
+          name: schoolName,
+        },
+        type: "teacher",
+        active: true,
+        code: generateInviteCode(schoolName),
+      };
+
+      await addDoc(collection(db, "invites"), teacherInvite);
+
+      // create student invite code
+
+      const studentInvite = {
+        school: {
+          id: newSchoolDoc.id,
+          name: schoolName,
+        },
+        type: "student",
+        active: true,
+        code: generateInviteCode(schoolName),
+      };
+
+      await addDoc(collection(db, "invites"), studentInvite);
+
+      console.log("made school and invites");
+      closeModal();
     } catch (error) {
       console.log(error);
     }
   };
 
-  const logStuff = () => {
-    console.log(schools);
-  };
+  const [creatingSchool, setCreatingSchool] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  if (user && user.type === "superadmin")
-    return (
-      <div className="flex-1 flex justify-center p-10">
-        <div className="flex flex-col gap-4">
-          <h1 className="">Welcome Super Admin</h1>
+  return (
+    <Transition appear show={createSchoolModalIsOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => {
+          // if (!creatingQuiz)
+          closeModal();
+        }}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
 
-          <button className="btn" onClick={logStuff}>
-            Log stuff
-          </button>
-
-          {/* CREATE SCHOOL */}
-          <div className="p-6 rounded-md shadow-md flex flex-col gap-4">
-            <h1 className="">Create school</h1>
-            <input
-              type="text"
-              className="p-2 outline-none border"
-              value={schoolName}
-              onChange={(e) => setSchoolName(e.target.value)}
-              placeholder={`Enter schools name`}
-            />
-
-            <input
-              type="number"
-              className="p-2 outline-none border"
-              value={licenseLimit}
-              onChange={(e) => setLicenseLimit(e.target.value)}
-              placeholder={`Enter license limit`}
-            />
-
-            <span className="">Add admins</span>
-
-            <button className="btn" onClick={createSchool}>
-              Create school
-            </button>
-          </div>
-
-          {/* VIEW SCHOOLS */}
-          <h1 className="font-bold">Schools</h1>
-          {schools &&
-            schools.map((school: School) => {
-              return (
-                <Link href={`/superadmin/schools/${school.id}`} key={school.id}>
-                  <div className="p-2 border">
-                    <h1 className="">{school.name}</h1>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              {creatingSchool ? (
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex flex-col items-center gap-2">
+                    <span className="loading loading-spinner loading-lg"></span>
+                    <h1 className="">Creating School. Please wait a moment</h1>
                   </div>
-                </Link>
-              );
-            })}
+                </Dialog.Panel>
+              ) : (
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Create School
+                  </Dialog.Title>
 
-          {schools.length === 0 ? (
-            <h1 className="">No schools to show...</h1>
-          ) : null}
-          {/* VIEW STUDENTS */}
+                  {/* CREATE QUIZ */}
+                  <div className="flex flex-col gap-4 mt-4">
+                    <div className="flex flex-col gap-1">
+                      <label className="text-sm font-light">School Name</label>
+                      <input
+                        type="text"
+                        className="p-2 outline-none border"
+                        value={schoolName}
+                        onChange={(e) => setSchoolName(e.target.value)}
+                        placeholder={`Enter schools name`}
+                      />
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                      <label className="text-sm font-light">
+                        License Limit
+                      </label>
+                      <input
+                        type="number"
+                        className="p-2 outline-none border"
+                        value={licenseLimit}
+                        onChange={(e) => setLicenseLimit(e.target.value)}
+                        placeholder={`Enter license limit`}
+                      />
+                    </div>
+
+                    <button className="btn  btn-primary" onClick={createSchool}>
+                      Create School
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              )}
+            </Transition.Child>
+          </div>
         </div>
-      </div>
-    );
+      </Dialog>
+    </Transition>
+  );
 }
--- pages/superadmin/schools/[schoolId].tsx
+++ pages/superadmin/schools/[schoolId].tsx
@@ -1,6 +1,6 @@
 import { AuthContext } from "@/context/AuthContext";
 import { db } from "@/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 
@@ -16,17 +16,28 @@ export default function SuperAdminSchool() {
   const [adminName, setAdminName] = useState<any>("");
   const [adminEmail, setAdminEmail] = useState<any>("");
 
+  const [invites, setInvites] = useState<any>([]);
+
   useEffect(() => {
-    const fetchSchool = async () => {
+    const fetchSchoolAndInvites = async () => {
       try {
         let schoolDoc = await getDoc(doc(db, "schools", schoolId as string));
         setSchool({ id: schoolDoc.id, ...schoolDoc.data() });
+
+        let invitesSnapshot = await getDocs(collection(db, "invites"));
+
+        let invites: any = [];
+        invitesSnapshot.forEach((inviteDoc) => {
+          if (inviteDoc.data().school.id === schoolDoc.id)
+            invites.push({ id: inviteDoc.id, ...inviteDoc.data() });
+        });
+        setInvites(invites);
       } catch (error) {
         console.log(error);
       }
     };
 
-    if (schoolId) fetchSchool();
+    if (schoolId) fetchSchoolAndInvites();
   }, [schoolId]);
 
   const addAdmin = async () => {
@@ -39,70 +50,102 @@ export default function SuperAdminSchool() {
 
   if (schoolId && school)
     return (
-      <div className="p-10 flex justify-center">
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <h1 className="">school: {school.name}</h1>
+      <div className="p-10 flex flex-col gap-4">
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-4 bg-white p-4 rounded-md shadow-md flex-1">
+            <h1 className="text-xl font-bold">{school.name}</h1>
+            <h1 className="text-xl font-bold">
+              License: {school.students.length} used of {school.licenseLimit}
+            </h1>
             <span className="text-sm font-light">id: {school.id}</span>
-            <button className="btn" onClick={() => console.log(school)}>
+            <button className="btn w-fit" onClick={() => console.log(school)}>
               Log school info
             </button>
           </div>
 
-          <h1 className="font-bold">School admins: </h1>
-          {school.admins &&
-            school.admins.map((admin: any, i: any) => {
-              return (
-                <div className="p-2 border flex items-center gap-6" key={i}>
-                  <span className="">Name: {admin.name}</span>
-                  <span className="">Email: {admin.email}</span>
-                  <button className="btn">Edit/remove</button>
-                </div>
-              );
-            })}
-          {!school.admins || school.admins.length === 0 ? (
-            <h1 className="">No admins...</h1>
-          ) : null}
+          <div className="flex flex-col gap-4 bg-white p-4 rounded-md shadow-md flex-1">
+            <h1 className="font-bold">School admins: </h1>
+            {school.admins &&
+              school.admins.map((admin: any, i: any) => {
+                return (
+                  <div
+                    className="p-2 border flex items-center justify-between gap-6"
+                    key={i}
+                  >
+                    <span className="">Name: {admin.name}</span>
+                    <span className="">Email: {admin.email}</span>
+                    {/* <button className="btn">Edit/remove</button> */}
+                  </div>
+                );
+              })}
+            {!school.admins || school.admins.length === 0 ? (
+              <h1 className="">No admins...</h1>
+            ) : null}
+          </div>
+        </div>
 
-          <div className="flex flex-col gap-2 border shadow-md mt-4">
-            <h1 className="font-bold">
-              Add admin{" "}
-              <span className="text-orange-500">
-                To be worked on...firebase admin
-              </span>
-            </h1>
-            <input
-              type="text"
-              className="p-2"
-              value={adminName}
-              onChange={(e) => setAdminName(e.target.value)}
-              placeholder={`Enter admin name`}
-            />
-            <input
-              type="email"
-              className="p-2"
-              value={adminEmail}
-              onChange={(e) => setAdminEmail(e.target.value)}
-              placeholder={`Enter admin email`}
-            />
-            <button className="btn" onClick={addAdmin}>
-              Add admin
-            </button>
+        <div className="flex gap-4">
+          {/* INVITATION CODE */}
+          <div className="p-4 rounded-md bg-white shadow-md flex flex-col gap-4">
+            <h1 className="text-xl font-bold">Invitation Codes</h1>
+
+            <div className="flex flex-col gap-2">
+              <h1 className="text-lg font-medium">Student</h1>
+              {invites &&
+                invites.map((invite: any) => {
+                  if (invite.type !== "student") return null;
+
+                  return (
+                    <h1 className="text-xl font-medium p-4 border-2 w-fit">
+                      {invite.code}
+                    </h1>
+                  );
+                })}
+              <h4 className="text-sm font-light">
+                Send this code to your students to let them join
+              </h4>
+            </div>
+            {/* TEACHER INVITE CODE */}
+
+            <div className="flex flex-col gap-2">
+              <h1 className="text-lg font-medium">Teacher</h1>
+              {invites &&
+                invites.map((invite: any) => {
+                  if (invite.type !== "teacher") return null;
+
+                  return (
+                    <h1 className="text-xl font-medium p-4 border-2 w-fit">
+                      {invite.code}
+                    </h1>
+                  );
+                })}
+              <h4 className="text-sm font-light">
+                Send this code to other staff to join as admins
+              </h4>
+            </div>
           </div>
 
-          <h1 className="font-bold">Students: </h1>
-          {school.students &&
-            school.students.map((student: any) => {
-              return (
-                <div
-                  className="flex items-center gap-4 p-2 border"
-                  key={student.id}
-                >
-                  <span className="">{student.email}</span>
-                  <span className="">{student.name}</span>
-                </div>
-              );
-            })}
+          {/* STUDENTS */}
+          <div className="flex flex-col gap-4 bg-white p-4 rounded-md shadow-md flex-1">
+            <h1 className="font-bold">Students: </h1>
+
+            {school.students &&
+              school.students.map((student: any) => {
+                return (
+                  <div
+                    className="flex items-center gap-4 p-2 border"
+                    key={student.id}
+                  >
+                    <span className="">{student.email}</span>
+                    <span className="">{student.name}</span>
+                  </div>
+                );
+              })}
+
+            {!school.students || school.students.length === 0 ? (
+              <h1 className="">No students</h1>
+            ) : null}
+          </div>
         </div>
       </div>
     );
