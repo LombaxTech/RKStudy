472d62772987d17a6ee7ab753f5e983dc084ff9d
Rakib <rakibkhan@live.co.uk>
2024-09-26 00:17:42 +0100

    add plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/context/AuthContext";
 import Layout from "@/components/Layout";
+import PlausibleProvider from "next-plausible";
+
+const domain = `rkstudy.co.uk`;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <div data-theme="light">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
-    </AuthProvider>
+    <PlausibleProvider domain={domain}>
+      <AuthProvider>
+        <div data-theme="light">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </AuthProvider>
+    </PlausibleProvider>
   );
 }
