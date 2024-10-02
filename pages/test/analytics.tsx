4882a3e5dceb79adcdb76048d2cfe6ff0e9974ad
Rakib <rakibkhan@live.co.uk>
2024-10-02 16:47:06 +0100

    test analytics page update

--- pages/test/analytics.tsx
+++ pages/test/analytics.tsx
@@ -3,9 +3,17 @@ import { usePlausible } from "next-plausible";
 export default function Analytics() {
   const plausible = usePlausible();
 
+  let demoGen = "demo-quiz-gen"; // 3
+  let quizGen = "quiz-gen"; // 4
+  let demoLimitHit = "demo-limit-hit";
+  let limitHit = "limit-hit"; // 1
+  let signup = "";
+
   const fireEvent = async () => {
-    plausible("gen-quiz-test");
-    console.log("fired event");
+    let event = limitHit;
+
+    plausible(event);
+    console.log(event);
   };
 
   return (
