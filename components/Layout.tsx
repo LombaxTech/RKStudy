4ac7acbfb4da6a7b1a2088a54c40302bb224643d
Rakib <rakibkhan@live.co.uk>
2024-10-03 17:35:46 +0100

    add a dev message on top of nav

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import { AuthContext } from "@/context/AuthContext";
 import React, { useContext, useEffect } from "react";
 import Navbar from "./Navbar";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: any }) {
   const { user, userLoading } = useContext(AuthContext);
@@ -12,6 +13,7 @@ export default function Layout({ children }: { children: any }) {
   const isChatPage = pathname === "/ai-tutor";
   const isLandingPage = !userLoading && !user && pathname === "/";
   const isAuthPage = pathname === "/signup" || pathname === "/signin";
+  const isDemo = pathname === "/demo";
 
   return (
     <div
@@ -19,6 +21,21 @@ export default function Layout({ children }: { children: any }) {
         isChatPage ? "max-h-screen" : ""
       }`}
     >
+      {!isLandingPage && (
+        <div className="p-2 bg-primary text-primary-content">
+          <Link
+            href={"/feedback"}
+            className="flex items-center gap-2 justify-center"
+          >
+            <h1 className="text-center font-bold text-lg">
+              We're still in development!
+            </h1>
+            <h1 className="text-center text-sm underline">
+              Help us make you the best study platform
+            </h1>
+          </Link>
+        </div>
+      )}
       {isLandingPage ? null : <Navbar />}
       <div
         className={`flex-1 flex flex-col ${
