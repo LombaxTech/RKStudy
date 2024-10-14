38d6caf4239bceacdb804b9ca631d74757f4d9cb
Rakib <rakibkhan@live.co.uk>
2024-10-14 01:13:01 +0100

    add some spec data

---
+++ specData.ts
@@ -0,0 +1,470 @@
+export const mathGCSESpec = {
+  topics: [
+    {
+      number: "1",
+      title: "Number Operations and Integers",
+      subtopics: [
+        {
+          number: "1.01",
+          title: "Calculations with integers",
+          description:
+            "Use non-calculator methods to calculate the sum, difference, product, and quotient of positive and negative whole numbers.",
+        },
+        {
+          number: "1.02",
+          title: "Whole number theory",
+          subtopics: [
+            {
+              number: "1.02a",
+              title: "Definitions and terms",
+              description:
+                "Understand and use terms such as odd, even, prime, factor, multiple, common factor, square, cube, and root.",
+            },
+            {
+              number: "1.02b",
+              title: "Prime numbers",
+              description:
+                "Identify prime numbers and express a whole number as a product of its prime factors.",
+            },
+            {
+              number: "1.02c",
+              title:
+                "Highest Common Factor (HCF) and Lowest Common Multiple (LCM)",
+              description:
+                "Find the HCF and LCM of two whole numbers by listing or from their prime factorisations.",
+            },
+          ],
+        },
+        {
+          number: "1.03",
+          title: "Combining arithmetic operations",
+          description:
+            "Know the conventional order for performing calculations involving brackets, four rules, powers, roots, and reciprocals.",
+        },
+        {
+          number: "1.04",
+          title: "Inverse operations",
+          description:
+            "Know that addition and subtraction, multiplication and division, and powers and roots are inverse operations and use this to simplify and check calculations.",
+        },
+      ],
+    },
+    {
+      number: "2",
+      title: "Fractions, Decimals and Percentages",
+      subtopics: [
+        {
+          number: "2.01",
+          title: "Fractions",
+          subtopics: [
+            {
+              number: "2.01a",
+              title: "Equivalent fractions",
+              description:
+                "Recognise and use equivalence between simple fractions and mixed numbers.",
+            },
+            {
+              number: "2.01b",
+              title: "Calculations with fractions",
+              description:
+                "Add, subtract, multiply, and divide simple fractions, including improper fractions and mixed numbers.",
+            },
+            {
+              number: "2.01c",
+              title: "Fractions of a quantity",
+              description:
+                "Calculate a fraction of a quantity and express one quantity as a fraction of another.",
+            },
+          ],
+        },
+        {
+          number: "2.02",
+          title: "Decimal fractions",
+          subtopics: [
+            {
+              number: "2.02a",
+              title: "Decimals and fractions",
+              description:
+                "Express a simple fraction as a decimal and vice versa without a calculator.",
+            },
+            {
+              number: "2.02b",
+              title: "Addition, subtraction and multiplication of decimals",
+              description:
+                "Add, subtract, and multiply decimals, including negative decimals, without a calculator.",
+            },
+            {
+              number: "2.02c",
+              title: "Division of decimals",
+              description:
+                "Divide a decimal by a whole number or another decimal, including negative decimals, without a calculator.",
+            },
+          ],
+        },
+        {
+          number: "2.03",
+          title: "Percentages",
+          subtopics: [
+            {
+              number: "2.03a",
+              title: "Percentage conversions",
+              description:
+                "Convert between fractions, decimals, and percentages.",
+            },
+            {
+              number: "2.03b",
+              title: "Percentage calculations",
+              description:
+                "Calculate a percentage of a quantity and express one quantity as a percentage of another.",
+            },
+            {
+              number: "2.03c",
+              title: "Percentage change",
+              description:
+                "Increase or decrease a quantity by a percentage and apply it to simple value problems and interest calculations.",
+            },
+          ],
+        },
+        {
+          number: "2.04",
+          title: "Ordering fractions, decimals, and percentages",
+          subtopics: [
+            {
+              number: "2.04a",
+              title: "Ordinality",
+              description:
+                "Order integers, fractions, decimals, and percentages.",
+            },
+            {
+              number: "2.04b",
+              title: "Symbols",
+              description: "Use comparison symbols like >, <, and =.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "3",
+      title: "Indices and Surds",
+      subtopics: [
+        {
+          number: "3.01",
+          title: "Powers and roots",
+          subtopics: [
+            {
+              number: "3.01a",
+              title: "Index notation",
+              description:
+                "Use positive and negative integer indices to represent powers and roots.",
+            },
+            {
+              number: "3.01b",
+              title: "Calculation and estimation of powers and roots",
+              description:
+                "Calculate integer powers, roots, and fractional powers.",
+            },
+            {
+              number: "3.01c",
+              title: "Laws of indices",
+              description:
+                "Know and apply the laws of indices in simplifying expressions.",
+            },
+          ],
+        },
+        {
+          number: "3.02",
+          title: "Standard form",
+          subtopics: [
+            {
+              number: "3.02a",
+              title: "Standard form notation",
+              description:
+                "Interpret, convert, and order numbers in standard form.",
+            },
+            {
+              number: "3.02b",
+              title: "Calculations in standard form",
+              description:
+                "Use a calculator or manual methods to perform calculations with numbers in standard form.",
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      number: "4",
+      title: "Approximation and Estimation",
+      subtopics: [
+        {
+          number: "4.01",
+          title: "Approximation and estimation",
+          subtopics: [
+            {
+              number: "4.01a",
+              title: "Rounding",
+              description:
+                "Round numbers to the nearest whole number, significant figures, or decimal places.",
+            },
+            {
+              number: "4.01b",
+              title: "Estimation",
+              description:
+                "Estimate or check the result of calculations using approximations.",
+            },
+            {
+              number: "4.01c",
+              title: "Upper and lower bounds",
+              description:
+                "Use inequality notation to write down error intervals and calculate upper and lower bounds.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "5",
+      title: "Ratio, Proportion and Rates Of Change",
+      subtopics: [
+        {
+          number: "5.01",
+          title: "Calculations with ratio",
+          subtopics: [
+            {
+              number: "5.01a",
+              title: "Equivalent ratios",
+              description: "Find and simplify ratios of quantities.",
+            },
+            {
+              number: "5.01b",
+              title: "Division in a given ratio",
+              description:
+                "Split a quantity into parts given the ratio of the parts.",
+            },
+            {
+              number: "5.01c",
+              title: "Ratios and fractions",
+              description:
+                "Interpret a ratio of two parts as a fraction of a whole.",
+            },
+            {
+              number: "5.01d",
+              title: "Solve ratio and proportion problems",
+              description: "Solve simple ratio and proportion problems.",
+            },
+          ],
+        },
+        {
+          number: "5.02",
+          title: "Direct and inverse proportion",
+          subtopics: [
+            {
+              number: "5.02a",
+              title: "Direct proportion",
+              description:
+                "Solve problems involving quantities in direct proportion.",
+            },
+            {
+              number: "5.02b",
+              title: "Inverse proportion",
+              description:
+                "Solve problems involving quantities in inverse proportion.",
+            },
+          ],
+        },
+        {
+          number: "5.03",
+          title: "Discrete growth and decay",
+          subtopics: [
+            {
+              number: "5.03a",
+              title: "Growth and decay",
+              description:
+                "Solve problems involving growth and decay such as compound interest and depreciation.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "6",
+      title: "Algebra",
+      subtopics: [
+        {
+          number: "6.01",
+          title: "Algebraic expressions",
+          subtopics: [
+            {
+              number: "6.01a",
+              title: "Algebraic terminology and proofs",
+              description:
+                "Understand and use concepts and vocabulary of expressions, equations, formulae, inequalities, terms, and factors.",
+            },
+            {
+              number: "6.01b",
+              title: "Collecting like terms",
+              description:
+                "Simplify algebraic expressions by collecting like terms.",
+            },
+            {
+              number: "6.01c",
+              title: "Simplifying products and quotients",
+              description:
+                "Simplify algebraic products and quotients using the laws of indices.",
+            },
+            {
+              number: "6.01d",
+              title: "Multiplying out brackets",
+              description:
+                "Simplify algebraic expressions by multiplying out brackets.",
+            },
+            {
+              number: "6.01e",
+              title: "Factorising",
+              description:
+                "Factorise quadratic expressions and take out common factors.",
+            },
+          ],
+        },
+        {
+          number: "6.02",
+          title: "Algebraic formulae",
+          subtopics: [
+            {
+              number: "6.02a",
+              title: "Formulate algebraic expressions",
+              description:
+                "Formulate simple formulae and expressions from real-world contexts.",
+            },
+            {
+              number: "6.02b",
+              title: "Substitute numerical values into formulae",
+              description:
+                "Substitute values into simple and complex formulae, including powers and roots.",
+            },
+            {
+              number: "6.02c",
+              title: "Change the subject of a formula",
+              description: "Rearrange formulae to change the subject.",
+            },
+          ],
+        },
+        {
+          number: "6.03",
+          title: "Algebraic equations",
+          subtopics: [
+            {
+              number: "6.03a",
+              title: "Linear equations",
+              description:
+                "Solve linear equations in one unknown algebraically.",
+            },
+            {
+              number: "6.03b",
+              title: "Quadratic equations",
+              description:
+                "Solve quadratic equations by factorising or using the quadratic formula.",
+            },
+            {
+              number: "6.03c",
+              title: "Simultaneous equations",
+              description:
+                "Solve two simultaneous equations in two variables algebraically.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "7",
+      title: "Graphs of Equations and Functions",
+      subtopics: [
+        {
+          number: "7.01",
+          title: "Graphs of equations and functions",
+          subtopics: [
+            {
+              number: "7.01a",
+              title: "x- and y-coordinates",
+              description:
+                "Work with x- and y-coordinates in all four quadrants.",
+            },
+            {
+              number: "7.01b",
+              title: "Graphs of equations and functions",
+              description:
+                "Plot graphs of linear and quadratic functions using a table of values.",
+            },
+          ],
+        },
+        {
+          number: "7.02",
+          title: "Straight line graphs",
+          subtopics: [
+            {
+              number: "7.02a",
+              title: "Straight line graphs",
+              description:
+                "Find and interpret the gradient and intercept of straight lines using y=mx+c.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "8",
+      title: "Basic Geometry",
+      subtopics: [
+        {
+          number: "8.01",
+          title: "Conventions, notation, and terms",
+          subtopics: [
+            {
+              number: "8.01a",
+              title: "2D and 3D shapes",
+              description:
+                "Use the terms points, lines, segments, vertices, and edges.",
+            },
+          ],
+        },
+        {
+          number: "8.03",
+          title: "Angles",
+          subtopics: [
+            {
+              number: "8.03a",
+              title: "Angles at a point",
+              description:
+                "Know and use the fact that the sum of the angles at a point is 360°.",
+            },
+            {
+              number: "8.03b",
+              title: "Angles on a line",
+              description:
+                "Know that the sum of angles on a straight line is 180°.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "9",
+      title: "Congruence and Similarity",
+      subtopics: [
+        {
+          number: "9.01",
+          title: "Plane isometric transformations",
+          subtopics: [
+            {
+              number: "9.01a",
+              title: "Reflection",
+              description:
+                "Reflect a shape in a given mirror line and identify the mirror line.",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
