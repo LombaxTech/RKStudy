b00fbb8a512dde202e814d7ee768b982300495bc
Rakib <rakibkhan@live.co.uk>
2024-12-19 23:59:41 +0000

    add an add spec modal

---
+++ components/syllabus/AddSpecModal.tsx
@@ -0,0 +1,117 @@
+import { AuthContext } from "@/context/AuthContext";
+import { db } from "@/firebase";
+import { availableSpecs } from "@/lib/specData/specs";
+import { User } from "@/lib/types";
+import { updateDoc } from "firebase/firestore";
+import { doc } from "firebase/firestore";
+import { Dialog, Transition } from "@headlessui/react";
+import { usePlausible } from "next-plausible";
+import { Fragment, useContext, useState } from "react";
+
+export default function AddSpecModal({
+  addSpecModalIsOpen,
+  setAddSpecModalIsOpen,
+}: {
+  addSpecModalIsOpen: any;
+  setAddSpecModalIsOpen: any;
+}) {
+  const plausible = usePlausible();
+
+  const closeModal = () => setAddSpecModalIsOpen(false);
+  const openModal = () => setAddSpecModalIsOpen(true);
+
+  const { user, setUser }: { user: User; setUser: any } =
+    useContext(AuthContext);
+
+  const [selectedSpec, setSelectedSpec] = useState<string>("");
+
+  const addSpec = async () => {
+    if (!selectedSpec) return;
+
+    const spec = availableSpecs.find((spec) => spec.id === selectedSpec);
+    if (!spec) return;
+
+    try {
+      let userPreviousSpecs = user.specs || [];
+      let userUpdatedSpecs = [
+        ...userPreviousSpecs,
+        {
+          ...spec,
+          studyPointConfidenceRatings: {},
+        },
+      ];
+
+      await updateDoc(doc(db, "users", user.uid as string), {
+        specs: userUpdatedSpecs,
+      });
+
+      setUser({ ...user, specs: userUpdatedSpecs });
+      closeModal();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Transition appear show={addSpecModalIsOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/50" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <div className="flex flex-col gap-8">
+                  <h1 className="text-2xl font-bold">
+                    Add a syllabus that you're studying
+                  </h1>
+                  {/* AVAILABLE SYLLABUSES */}
+                  <div className="flex flex-col gap-4">
+                    <select
+                      className="p-2 outline-none border border-gray-300 rounded-md"
+                      value={selectedSpec}
+                      onChange={(e) => setSelectedSpec(e.target.value)}
+                    >
+                      <option value="">Select a syllabus</option>
+                      {availableSpecs.map((spec) => {
+                        // if (user.specs.includes(spec.id)) {
+                        //   return null;
+                        // }
+                        return <option value={spec.id}>{spec.title}</option>;
+                      })}
+                    </select>
+                    <button
+                      className="btn btn-primary"
+                      disabled={!selectedSpec}
+                      onClick={addSpec}
+                    >
+                      Add to your list
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
--- pages/syllabus/index.tsx
+++ pages/syllabus/index.tsx
@@ -1,26 +1,72 @@
+import AddSpecModal from "@/components/syllabus/AddSpecModal";
+import { AuthContext } from "@/context/AuthContext";
+import { db } from "@/firebase";
+import { updateDoc } from "firebase/firestore";
+import { UserSpec } from "@/lib/types";
+import { doc } from "firebase/firestore";
 import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
 
 export default function Syllabus() {
+  const { user } = useContext(AuthContext);
+
+  const [specs, setSpecs] = useState<any[]>([]);
+  const [availableSpecs, setAvailableSpecs] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    let specs = user.specs || [];
+    setSpecs(specs);
+  }, [user]);
+
+  if (!user) return <div>Loading...</div>;
   return (
     <div className="flex-1 p-4 flex flex-col gap-2">
       <h1 className="text-2xl font-bold">Syllabuses</h1>
 
       {/* SYLLABUSES */}
       <div className="flex flex-col gap-2">
-        <div className="p-4 bg-white rounded-lg shadow-md flex items-center justify-between">
-          <h2 className="text-xl font-bold">CIE Biology AS Level 2024</h2>
+        {/* NO SPECS ADDED YET */}
+        {specs.length === 0 && <NoSpecsSelectedYet />}
 
-          <Link href="/syllabus/cie-biology-as-2024">
-            <button className="btn btn-primary btn-sm">View</button>
-          </Link>
-        </div>
+        {/* ADDED SPECS */}
+        {specs &&
+          specs.map((spec: UserSpec) => (
+            <div className="p-4 bg-white rounded-lg shadow-md flex items-center justify-between">
+              <h2 className="text-xl font-bold">{spec.title}</h2>
 
-        {/* {syllabuses.map((syllabus) => (
-          <div className="flex flex-col gap-2">
-            <h2 className="text-xl font-bold">{syllabus.name}</h2>
-          </div>
-        ))} */}
+              <Link href={`/syllabus/${spec.id}`}>
+                <button className="btn btn-primary btn-sm">View</button>
+              </Link>
+            </div>
+          ))}
       </div>
     </div>
   );
 }
+
+const NoSpecsSelectedYet = () => {
+  const [addSpecModalIsOpen, setAddSpecModalIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="p-4 bg-white rounded-lg shadow-md flex flex-col gap-2">
+        <h2 className="text-xl font-bold">No specs selected yet</h2>
+        <p className="text-sm text-gray-500">
+          No specs selected yet. Please select a spec to view the syllabus.
+        </p>
+        <button
+          className="btn btn-primary btn-sm w-fit"
+          onClick={() => setAddSpecModalIsOpen(true)}
+        >
+          Select a spec
+        </button>
+      </div>
+      <AddSpecModal
+        addSpecModalIsOpen={addSpecModalIsOpen}
+        setAddSpecModalIsOpen={setAddSpecModalIsOpen}
+      />
+    </>
+  );
+};
