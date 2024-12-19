b0cb2351d6ab3dece2b26a20327ceb09671a0693
Rakib <rakibkhan@live.co.uk>
2024-12-19 21:27:18 +0000

    bio cie syllabus data

---
+++ lib/specData/cie-biology-as-2024.ts
@@ -0,0 +1,178 @@
+// TOPICS -> SUBTOPICS -> POINTS
+
+export const cieBiologyAS2024Syllabus = {
+  id: "cie-biology-as-2024",
+  title: "CIE Biology AS Level 2024",
+  topics: [
+    {
+      number: "1",
+      title: "Cell Structure",
+      subtopics: [
+        {
+          number: "1.1",
+          title: "The microscrope in cell studies",
+          points: [
+            {
+              number: "1.1.1",
+              title:
+                "make temporary preparations of cellular material suitable for viewing with a light microscope",
+            },
+            {
+              number: "1.1.2",
+              title: "draw cells from microscope slides and photomicrographs",
+            },
+            {
+              number: "1.1.3",
+              title:
+                "calculate magnifications of images and actual sizes of specimens from drawings, photomicrographs and electron micrographs (scanning and transmission)",
+            },
+            {
+              number: "1.1.4",
+              title: "use an eyepiece graticule and stage micromet",
+            },
+          ],
+        },
+        {
+          number: "1.2",
+          title: "Cells as the basic units of living organisms",
+          points: [
+            {
+              number: "1.2.1",
+              title:
+                "recognise organelles and other cell structures found in eukaryotic cells and outline their structures and functions",
+            },
+            {
+              number: "1.2.2",
+              title:
+                "describe and interpret photomicrographs, electron micrographs and drawings of typical plant and animal cells",
+            },
+            {
+              number: "1.2.3",
+              title: "compare the structure of typical plant and animal cells",
+            },
+            {
+              number: "1.2.4",
+              title:
+                "state that cells use ATP from respiration for energy-requiring processes",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      number: "2",
+      title: "Biological molecules",
+      subtopics: [
+        {
+          number: "2.1",
+          title: "Testing for biological molecules",
+          points: [
+            {
+              number: "2.1.1",
+              title:
+                "describe and carry out the Benedict’s test for reducing sugars, the iodine test for starch, the emulsion test for lipids and the biuret test for proteins",
+            },
+            {
+              number: "2.1.2",
+              title:
+                "describe and carry out a semi-quantitative Benedict’s test on a reducing sugar solution by standardising the test and using the results (time to first colour change or comparison to colour standards) to estimate the concentration",
+            },
+            {
+              number: "2.1.3",
+              title:
+                "describe and carry out a test to identify the presence of non-reducing sugars, using acid hydrolysis and Benedict’s solution",
+            },
+          ],
+        },
+        {
+          number: "2.2",
+          title: "Carbohydrates and lipids",
+          points: [
+            {
+              number: "2.2.1",
+              title:
+                "describe and draw the ring forms of α-glucose and β-glucose",
+            },
+            {
+              number: "2.2.2",
+              title:
+                "define the terms monomer, polymer, macromolecule, monosaccharide, disaccharide and polysaccharide",
+            },
+            {
+              number: "2.2.3",
+              title:
+                "state the role of covalent bonds in joining smaller molecules together to form polymers",
+            },
+            {
+              number: "2.2.4",
+              title:
+                "state that glucose, fructose and maltose are reducing sugars and that sucrose is a non-reducing sugar",
+            },
+            {
+              number: "2.2.5",
+              title:
+                "describe the formation of a glycosidic bond by condensation, with reference to disaccharides, including sucrose, and polysaccharides",
+            },
+          ],
+        },
+        {
+          number: "2.3",
+          title: "Proteins",
+          points: [
+            {
+              number: "2.3.1",
+              title:
+                "describe and draw the general structure of an amino acid and the formation and breakage of a peptide bond",
+            },
+            {
+              number: "2.3.2",
+              title:
+                "explain the meaning of the terms primary structure, secondary structure, tertiary structure and quaternary structure of proteins",
+            },
+            {
+              number: "2.3.3",
+              title:
+                "describe the types of interaction that hold protein molecules in shape: hydrophobic interactions, hydrogen bonding, ionic bonding, covalent bonding, including disulfide bonds",
+            },
+            {
+              number: "2.3.4",
+              title:
+                "state that globular proteins are generally soluble and have physiological roles and fibrous proteins are generally insoluble and have structural roles",
+            },
+            {
+              number: "2.3.5",
+              title:
+                "describe the structure of a molecule of haemoglobin as an example of a globular protein, including the formation of its quaternary structure from two alpha (α) chains (α–globin), two beta (β) chains (β–globin) and a haem group",
+            },
+            {
+              number: "2.3.6",
+              title:
+                "relate the structure of haemoglobin to its function, including the importance of iron in the haem group",
+            },
+            {
+              number: "2.3.7",
+              title:
+                "describe the structure of a molecule of collagen as an example of a fibrous protein, and the arrangement of collagen molecules to form collagen fibres",
+            },
+            {
+              number: "2.3.8",
+              title:
+                "relate the structures of collagen molecules and collagen fibres to their function",
+            },
+          ],
+        },
+        {
+          number: "2.4",
+          title: "Water",
+          points: [
+            {
+              number: "2.4.1",
+              title:
+                "explain how hydrogen bonding occurs between water molecules and relate the properties of water to its roles in living organisms, limited to solvent action, high specific heat capacity and latent heat of vaporisation",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
