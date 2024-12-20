ff6b082f5901c4610f774b59f37afa2fdfe51213
Rakib <rakibkhan@live.co.uk>
2024-12-20 21:02:41 +0000

    edited meta title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>RKStudy - Generate Quizzes From Notes</title>
+        <title>RKStudy - Boost Your Grades With Efficient Studying</title>
       </Head>
       <body>
         <Main />
