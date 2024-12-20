e81a685ba32cecad542b446dddf307f3b762d469
Rakib <rakibkhan@live.co.uk>
2024-12-20 17:39:24 +0000

    add extra spec

--- lib/specData/specs.ts
+++ lib/specData/specs.ts
@@ -3,4 +3,8 @@ export const availableSpecs = [
     id: "cie-biology-as-2024",
     title: "CIE Biology AS Level 2024",
   },
+  {
+    id: "cie-biology-a2-2024",
+    title: "CIE Biology A2 Level 2024",
+  },
 ];
