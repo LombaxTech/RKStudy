0f1d066e251906018dbc47c1e4ff9e9451a5623b
Rakib <rakibkhan@live.co.uk>
2024-12-21 13:32:13 +0000

    clean up code

--- pages/syllabus/index.tsx
+++ pages/syllabus/index.tsx
@@ -27,53 +27,32 @@ export default function Syllabus() {
   if (!user) return <div>Loading...</div>;
   return (
     <>
-      <div className="flex-1 p-4 flex flex-col gap-2">
-        {/* TITLE AND ADD SPEC BUTTON */}
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl font-bold">My Subjects</h1>
-
-          {specs.length > 0 && (
+      {specs.length === 0 && <NoSpecsSelectedYet />}
+      {specs.length > 0 && (
+        <div className="flex-1 p-4 flex flex-col gap-2 md:w-1/2 md:mx-auto md:gap-14 pt-10">
+          {/* TITLE AND ADD SPEC BUTTON */}
+          <div className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold">My Subjects</h1>
             <button
               className="btn btn-primary btn-sm"
               onClick={() => setAddSubjectModalIsOpen(true)}
             >
               Add a new subject
             </button>
-          )}
-        </div>
-
-        {/* SYLLABUSES */}
-        <div className="flex flex-col gap-2">
-          {/* NO SPECS ADDED YET */}
-          {specs.length === 0 && <NoSpecsSelectedYet />}
-
-          {/* ADDED SPECS */}
-          {specs &&
-            specs.map((spec: UserSpec) => (
-              <div
-                key={spec.id}
-                className="p-4 bg-white rounded-lg shadow-md flex items-center justify-between"
-              >
-                <h2 className="text-xl font-bold">{spec.title}</h2>
-
-                <div className="flex items-center gap-2">
-                  <Link href={`/syllabus/${spec.spec}/${spec.id}`}>
-                    <button className="btn btn-primary btn-sm">View</button>
-                  </Link>
-                  <button
-                    className="btn btn-secondary btn-sm"
-                    onClick={() => {
-                      setSubjectToRemove(spec);
-                      setRemoveSubjectModalIsOpen(true);
-                    }}
-                  >
-                    Remove
-                  </button>
-                </div>
-              </div>
+          </div>
+          {/* SUBJECTS */}
+          <div className="flex flex-col gap-2">
+            {specs.map((spec: UserSpec) => (
+              <SpecCard
+                spec={spec}
+                setSubjectToRemove={setSubjectToRemove}
+                setRemoveSubjectModalIsOpen={setRemoveSubjectModalIsOpen}
+              />
             ))}
+          </div>
         </div>
-      </div>
+      )}
+
       <AddSubjectModal
         addSubjectModalIsOpen={addSubjectModalIsOpen}
         setAddSubjectModalIsOpen={setAddSubjectModalIsOpen}
@@ -87,27 +66,57 @@ export default function Syllabus() {
   );
 }
 
+const SpecCard = ({
+  spec,
+  setSubjectToRemove,
+  setRemoveSubjectModalIsOpen,
+}: {
+  spec: UserSpec;
+  setSubjectToRemove: any;
+  setRemoveSubjectModalIsOpen: any;
+}) => {
+  return (
+    <>
+      <div className="p-4 bg-white rounded-lg shadow-md flex items-center justify-between">
+        <h2 className="text-xl font-bold">{spec.title}</h2>
+
+        <div className="flex items-center gap-2">
+          <Link href={`/syllabus/${spec.spec}/${spec.id}`}>
+            <button className="btn btn-primary btn-sm">View</button>
+          </Link>
+          <button
+            className="btn btn-secondary btn-sm"
+            onClick={() => {
+              setSubjectToRemove(spec);
+              setRemoveSubjectModalIsOpen(true);
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const NoSpecsSelectedYet = () => {
   const [addSubjectModalIsOpen, setAddSubjectModalIsOpen] = useState(false);
 
   return (
-    <>
-      <div className="p-4 bg-white rounded-lg shadow-md flex flex-col gap-2">
-        <h2 className="text-xl font-bold">No subjects selected yet</h2>
-        <p className="text-sm text-gray-500">
-          No subjects selected yet. Please add a subject to get started.
-        </p>
-        <button
-          className="btn btn-primary btn-sm w-fit"
-          onClick={() => setAddSubjectModalIsOpen(true)}
-        >
-          Add a subject
-        </button>
-      </div>
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold">No subjects added yet</h1>
+      <img src={"/math.svg"} alt="math" className="w-[300px]" />
+      <button
+        className="btn btn-primary w-fit mt-10"
+        onClick={() => setAddSubjectModalIsOpen(true)}
+      >
+        Add a subject to get started
+      </button>
+
       <AddSubjectModal
         addSubjectModalIsOpen={addSubjectModalIsOpen}
         setAddSubjectModalIsOpen={setAddSubjectModalIsOpen}
       />
-    </>
+    </div>
   );
 };
